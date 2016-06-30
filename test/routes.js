cce2c946527fe5961b7fbd2e95c1c304e36430fd
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-06-30 18:08:19 +0300

    From async loops to forEach.

--- test/routes.js
+++ test/routes.js
@@ -55,8 +55,11 @@ describe('Routing /users', function () {
         done();
     });
 
-    async.each(names_ok, function (item, callback) {
-        it('Should add users with proper name: ' + item, function (done) {
+    // --------------------------------------------------------------------------------------
+    // POST /users, 200
+
+    names_ok.forEach(function (item) {
+        it('Should add user with proper name: ' + item, function (done) {
             var body = {
                 name: item,
                 password: pass_ok[0],
@@ -73,12 +76,11 @@ describe('Routing /users', function () {
                     }
                     done();
                 });
-            callback();
         });
     });
 
-    async.each(pass_ok, function (item, callback) {
-        it('Should add users with proper password: ' + item, function (done) {
+    pass_ok.forEach(function (item) {
+        it('Should add user with proper password: ' + item, function (done) {
             var body = {
                 name: rstr.generate({length: 6, charset: 'alphabetic'}),
                 password: item,
@@ -95,12 +97,11 @@ describe('Routing /users', function () {
                     }
                     done();
                 });
-            callback();
         });
     });
 
-    async.each(email_ok, function (item, callback) {
-        it('Should add users with proper email: ' + item, function (done) {
+    email_ok.forEach(function (item) {
+        it('Should add user with proper email: ' + item, function (done) {
             var body = {
                 name: rstr.generate({length: 6, charset: 'alphabetic'}),
                 password: pass_ok[0],
@@ -117,11 +118,30 @@ describe('Routing /users', function () {
                     }
                     done();
                 });
-            callback();
         });
     });
 
-    it('Should get all users: ' + total_ok, function (done) {
+    // --------------------------------------------------------------------------------------
+    // GET /users, 200
+
+    it('Should get all users: ' + names_ok.length, function (done) {
+        dropDb();
+        names_ok.forEach(function (item) {
+            var body = {
+                name: item,
+                password: pass_ok[0],
+                email: rstr.generate({length: 4, charset: 'alphabetic'}) + '@test.test'
+            };
+            server
+                .post('/api/users')
+                .send(body)
+                .expect('Content-Type', /json/)
+                .end(function (err, res) {
+                    if(err){
+                        throw err;
+                    }
+                });
+        });
         server
             .get('/api/users')
             .expect('Content-Type', /json/)
@@ -130,13 +150,16 @@ describe('Routing /users', function () {
                 if(err){
                     throw err;
                 }
-                should.equal(Object.keys(res.body).length, total_ok);
+                should.equal(Object.keys(res.body).length, names_ok.length);
                 done();
             });
     });
 
-    async.each(names_fail, function (item, callback) {
-        it('Should not add users with an improper name: ' + item, function (done) {
+    // --------------------------------------------------------------------------------------
+    // POST /users, 400
+
+    names_fail.forEach(function (item) {
+        it('Should not add user with an improper name: ' + item, function (done) {
             var body = {
                 name: item,
                 password: pass_ok[0],
@@ -153,12 +176,11 @@ describe('Routing /users', function () {
                     }
                     done();
                 });
-            callback();
         });
     });
 
-    async.each(pass_fail, function (item, callback) {
-        it('Should not add users with an improper password: ' + item, function (done) {
+    pass_fail.forEach(function (item) {
+        it('Should not add user with an improper password: ' + item, function (done) {
             var body = {
                 name: rstr.generate({length: 6, charset: 'alphabetic'}),
                 password: item,
@@ -175,12 +197,11 @@ describe('Routing /users', function () {
                     }
                     done();
                 });
-            callback();
         });
     });
 
-    async.each(email_fail, function (item, callback) {
-        it('Should not add users with an improper email: ' + item, function (done) {
+    email_fail.forEach(function (item) {
+        it('Should not add user with an improper email: ' + item, function (done) {
             var body = {
                 name: rstr.generate({length: 6, charset: 'alphabetic'}),
                 password: pass_ok[0],
@@ -197,7 +218,6 @@ describe('Routing /users', function () {
                     }
                     done();
                 });
-            callback();
         });
     });
 
