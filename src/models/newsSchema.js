049d4cb9deac87addf15f6e5a92fdd0795a8afb1
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-07-06 22:26:12 +0300

    Schema for news.

---
+++ src/models/newsSchema.js
@@ -0,0 +1,30 @@
+const mongoose      = require('mongoose');
+const Schema        = mongoose.Schema;
+
+const newsSchema    = new Schema({
+    _author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        default: 'Not defined'
+    },
+    body: {
+        type: String,
+        default: ''
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    modified: {
+        type: Date
+    },
+    published: {
+        type: Date
+    }
+}, { strict: true });
+
+module.exports = mongoose.model('NewsItem', newsSchema);
