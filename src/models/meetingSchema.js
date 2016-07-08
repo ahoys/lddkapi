bcdd8da53cdae972a242c76cd6e6af33674aa616
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-07-08 23:49:03 +0300

    Added abbreviation.

--- src/models/meetingSchema.js
+++ src/models/meetingSchema.js
@@ -2,6 +2,10 @@ const mongoose          = require('mongoose');
 const Schema            = mongoose.Schema;
 
 const MeetingSchema    = new Schema({
+    abbreviation: {
+        type: String,
+        required: true
+    },
     title: {
         type: String,
         required: true
