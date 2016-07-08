685542ecd8242d385b6a06d07db8266de3a76107
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-07-09 00:19:46 +0300

    Validation.

--- src/models/meetingSchema.js
+++ src/models/meetingSchema.js
@@ -4,14 +4,17 @@ const Schema            = mongoose.Schema;
 const MeetingSchema    = new Schema({
     abbreviation: {
         type: String,
+        validation: [/^(?=.*[A-Z])[A-Z0-9]{2,16}$/, 'Abbreviation must be between 2-16 and only A-Z, 0-9'],
         required: true
     },
     title: {
         type: String,
+        validation: [/^(?=.*[a-zA-Z])[a-zA-Z0-9äÄöÖåÅ! '"-.,*()]{3,48}$/, 'Must have a-z or A-Z, and be between 3-48.'],
         required: true
     },
     description: {
-        type: String
+        type: String,
+        validation: [/^(?=.*[a-zA-Z])[a-zA-Z0-9äÄöÖåÅ! '"-.,*()]{3,48}$/, 'Must have a-z or A-Z, and be between 3-48.']
     },
     date: {
         type: Date,
