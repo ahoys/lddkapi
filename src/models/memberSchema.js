637b1e32c3a7511499c258abe59071609cfd3385
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-07-08 23:17:53 +0300

    Added missing rank.

--- src/models/memberSchema.js
+++ src/models/memberSchema.js
@@ -14,6 +14,10 @@ const memberSchema  = new Schema({
         type: Date,
         default: Date.now
     },
+    rank: {
+        type: Schema.types.ObjectId,
+        ref: 'Rank'
+    },
     training: {
         type: Schema.Types.ObjectId,
         ref: 'Training'
