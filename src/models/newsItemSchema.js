b835d97fc7384bf795c3d006e5352d67bfe520e6
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-07-09 22:58:38 +0300

    Removed unnecessary _

--- src/models/newsItemSchema.js
+++ src/models/newsItemSchema.js
@@ -2,7 +2,7 @@ const mongoose          = require('mongoose');
 const Schema            = mongoose.Schema;
 
 const newsItemSchema    = new Schema({
-    _author: {
+    author: {
         type: Schema.Types.ObjectId,
         ref: 'User',
         required: true
