519af0788a9b48da61939fec2799631b8c326372
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-07-09 10:36:10 +0300

    Abbreviation regex.

--- src/models/ribbonSchema.js
+++ src/models/ribbonSchema.js
@@ -4,6 +4,7 @@ const Schema            = mongoose.Schema;
 const RibbonSchema    = new Schema({
     abbreviation: {
         type: String,
+        validation: [/^(?=.*[A-Z])[A-Z0-9ÄÖÅ]{2,16}$/, 'Abbreviation must be between 2-16 and only A-Z, 0-9'],
         required: true
     },
     title: {
