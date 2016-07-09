7c5f43238d67c9e1493f3d87689ecec074070a1f
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-07-09 22:51:15 +0300

    Implementation.

--- src/routes/trainings.js
+++ src/routes/trainings.js
@@ -7,25 +7,80 @@ module.exports = ((router) => {
     router.route('/trainings')
 
         .get((request, response) => {
-
+            Training.find({}, '-_id abbreviation title description', (err, trainings) => {
+                if(err){
+                    response.status(404).send(err);
+                    return false;
+                }
+                log('GET trainings successful.');
+                response.json(trainings);
+                return true;
+            })
         })
 
         .post((request, response) => {
-
+            const training = new Training({
+                abbreviation: request.body.abbreviation,
+                title: request.body.title,
+                description: request.body.description
+            });
+            training.save((err) => {
+                if(err){
+                    response.status(400).send(err);
+                    return false;
+                }
+                log('POST training successful.');
+                response.json({ message: 'A new training added.' });
+                return true;
+            });
         });
 
     router.route('/trainings/:training_abbreviation')
 
         .get((request, response) => {
-
+            Training.findOne(request.param.abbreviation, '-_id, abbreviation title description', (err, training) => {
+                if(err){
+                    response.status(404).send(err);
+                    return false;
+                }
+                log('GET ', training.title, ' successful.');
+                response.json(training);
+                return true;
+            });
         })
 
         .put((request, response) => {
-
+            Training.findOne(request.param.abbreviation, (err, training) => {
+                if(err){
+                    response.status(400).send(err);
+                    return false;
+                }
+                training.abbreviation = request.body.abbreviation;
+                training.title = request.body.title;
+                training.description = request.body.description;
+                training.save((err) => {
+                    if(err){
+                        response.status(400).send(err);
+                        return false;
+                    }
+                    log('PUT ', training.title, ' successful.');
+                    response.json({ message: 'The requested training was modified.' });
+                    return true;
+                });
+            });
         })
 
         .delete((request, response) => {
-
+            Training.remove({
+                abbreviation: request.params.abbreviation
+            }, (err, training) => {
+                if(err){
+                    response.status(404).send(err);
+                    return false;
+                }
+                log('DELETE ', training.title, ' successful.');
+                response.json({ message: 'The requested training was removed.' });
+                return true;
+            });
         });
-
 });
