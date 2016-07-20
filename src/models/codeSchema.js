0ec08dce82be4adbd331c0a9dc9159866798d2cf
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-07-20 22:22:56 +0300

    Initial implementation.

---
+++ src/models/codeSchema.js
@@ -0,0 +1,23 @@
+const mongoose      = require('mongoose');
+const Schema        = mongoose.Schema;
+
+const CodeSchema    = new Schema({
+    value: {
+        type: String,
+        required: true
+    },
+    redirectUri: {
+        type: String,
+        required: true
+    },
+    userId: {
+        type: String,
+        required: true
+    },
+    clientId: {
+        type: String,
+        required: true
+    }
+}, { strict: true });
+
+module.exports = mongoose.model('Code', CodeSchema);
