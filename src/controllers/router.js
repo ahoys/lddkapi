f85d39761d2c56e4067c8b0c1570f9790ee3ea0a
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-07-31 16:33:29 +0300

    Moved localization under db.

--- src/controllers/router.js
+++ src/controllers/router.js
@@ -1,6 +1,5 @@
 const debug         = require('../../debug');
-const express       = require('express');
-const config_app    = require('config').get('Application');
+const config_db     = require('config').get('Database');
 
 module.exports = ((express) => {
 
@@ -12,7 +11,7 @@ module.exports = ((express) => {
      * If the request does not specify localization or is not supported, default localization is used instead.
      */
     router.use((request, response, next) => {
-        request.localization = config_app.get('router.localization').indexOf(request.header('Accept-Language')) !== -1
+        request.localization = config_db.get('localization').indexOf(request.header('Accept-Language')) !== -1
             ? request.header('Accept-Language')
             : 'en' ;
         next();
