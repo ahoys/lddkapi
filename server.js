c26e90aaf8bdf8a5b4673b0710f0ee16190ce622
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-07-31 16:24:29 +0300

    Re-organized server.

--- server.js
+++ server.js
@@ -1,40 +1,38 @@
 require('babel-core/register');
-const debug             = require('./debug');
 
-// DATABASE ---------------------------------------------------
+// Base setup.
+const config_app        = require('config').get('Application');
 const config_db         = require('config').get('Database');
+const debug             = require('./debug');
 const mongoose          = require('mongoose');
+const express           = require('express');
+const bodyParser        = require('body-parser');
+const passport          = require('passport');
+const app               = express();
+const port              = process.env.PORT || config_app.get('port');
+const router            = require('./src/controllers/router')(express);
 
-// Create a database connection.
+// Db: create a new database connection.
 mongoose.connect(config_db.get('url'), config_db.get('port'));
 mongoose.connection.on('error', debug.error.bind(debug, 'Error: [Database connection failed.]'));
 
-// Verify the database connection.
+// Db: verify the connection.
 if(mongoose.connection.readyState !== 1 && mongoose.connection.readyState !== 2){
     debug.error('Error: [Mongoose connection failed with a code: ' + mongoose.connection.readyState + ']');
     process.exit();
 }
 
-// APPLICATION ------------------------------------------------
-const config_app        = require('config').get('Application');
-const express           = require('express');
-const app               = express();
-const port              = process.env.PORT || config_app.get('port');
-const router            = require('./src/controllers/router')(express);
-const passport          = require('passport');
-const bodyParser        = require('body-parser');
-
-// Initialize json parser.
+// App: initialize json parser.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Apply passport.
+// App: apply passport.
 app.use(passport.initialize());
 
-// Register routes.
+// App: register the router.
 app.use('/api', router);
 
-// Start the server.
+// App: start the server.
 app.listen(port);
 debug.log(
     '>>> ' +
