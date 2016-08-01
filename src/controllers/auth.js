5de9c9382abf008e7168722d258ed92e5067aff1
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-08-01 21:16:53 +0300

    Promise auth.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,3 +1,4 @@
+const log               = require('../../debug')('controllers:auth').debug;
 const passport          = require('passport');
 const BasicStrategy     = require('passport-http').BasicStrategy;
 const User              = require('../models/userSchema');
@@ -5,51 +6,55 @@ const Client            = require('../models/clientSchema');
 
 /**
  * Passport middleware.
- * Manages authentication for API endpoints.
+ * Manages authentication for users.
  */
 passport.use(new BasicStrategy((username, password, callback) => {
 
-    // Look for the requested user.
-    User.findOne({ username: username }, (err, user) => {
-
-        // If search fails, callback with an error.
-        if (err) { return callback(err); }
-
-        // If user cannot be found, callback.
-        if (!user) { return callback(null, false); }
-
-        // Verify the given password against the user in the database.
-        user.verifyPassword(password, (err, isMatch) => {
-
-            // Verifying failed.
-            if (err) { return callback(err); }
-
-            // If the password matches and everything went OK, return the user.
-            return isMatch ? callback(null, user) : callback(null, false) ;
+    User.findOne({ username: username })
+        .then((user) => {
+            if (user) {
+                user.verifyPassword(password, (err, isMatch) => {
+                    if (err) {
+                        log('Verifying the password failed.', true, err);
+                    }
+                    else {
+                        return isMatch ? callback(null, user) : callback(null, false) ;
+                    }
+                });
+            }
+            else {
+                log('User could not be found.');
+                return callback(null, false);
+            }
+        })
+        .catch((err) => {
+            log('User authenticating failed.', true, err);
+            return callback(null, false);
         });
-    });
 }));
 
 exports.isAuthenticated = passport.authenticate('basic', { session: false });
 
 /**
  * Passport middleware.
- * Manages authentication for the client
+ * Manages authentication for clients.
  */
-passport.use('client-basic', new BasicStrategy((username, password, callback) => {
+passport.use('client-basic', new BasicStrategy((name, password, callback) => {
 
-    // Look for the requested user.
-    Client.findOne({ username: username }, (err, client) => {
-
-        // If search fails, callback with an error.
-        if (err) { return callback(err); }
-
-        // Client not found or wrong password.
-        if (!client || client.secret !== password) { return callback(null, false); }
-
-        // Everything OK, return the client.
-        return callback(null, client);
-    });
+    Client.findOne({ name: name })
+        .then((client) => {
+            if (!client || client.secret !== password) {
+                log('Invalid logon with ' + client);
+                return callback(null, false);
+            }
+            else {
+                return callback(null, client);
+            }
+        })
+        .catch((err) => {
+            log('Client authenticating failed.', true, err);
+            return callback(null, false);
+        });
 }));
 
 exports.isClientAuthenticated = passport.authenticate('client-basic', { session: false });
