7ee00ba2ff3b71068887ee590ed2ca363b46bd6d
Ari Höysniemi <ari.hoysniemi@gmail.com>
2016-08-04 23:34:04 +0300

    Commentary.

--- src/models/tokenSchema.js
+++ src/models/tokenSchema.js
@@ -3,16 +3,20 @@ const Schema        = mongoose.Schema;
 
 /**
  * Access tokens for clients.
+ * The client uses an access token to make requests on behalf of the user.
  */
 const TokenSchema   = new Schema({
+    // Token value used when accessing the API on behalf of the user.
     value: {
         type: String,
         required: true
     },
+    // Owner User.
     userId: {
         type: String,
         required: true
     },
+    // Owner Client.
     clientId: {
         type: String,
         required: true
